8321bb619465922333a4cc04dc871d5487aa1f5f
Laurentiu Amagdei <Laurentiu Amagdei>
2016-04-12 15:55:14 +0300

    Everything

--- cloud_hw4/app.js
+++ cloud_hw4/app.js
@@ -5,8 +5,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var routes_to_ws = require('./routes/websocket');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/ws', routes_to_ws);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- cloud_hw4/routes/index.js
+++ cloud_hw4/routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var EventEmitter = require('events').EventEmitter;
+//messagebus for client requests
+var messageBus = new EventEmitter();
 
-
-router.get('/', function(req, res, next) {
-  res.send('bla bla');
+//renders index view
+router.get('/', function(req, res) {
+  res.render('index', {title: 'Bla bla'});
 });
+
+//subscribe request
+router.get('/messages', function(req, res){
+   var msgListener = function(res){
+       messageBus.once('message', function(data){
+          res.json(data); 
+       });
+   }
+   msgListener(res);
+});
+
+//emit message
+router.post('/messages', function(req, res){
+    messageBus.emit('message', req.body);
+    res.status(200).end();
+})
+
+
 
 router.post('/', function (req, res) {
   console.log(req.body);
